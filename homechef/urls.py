25518bb1279508a43338ee2cb8c4c79e56275d49
HuseinDawood <h.dawood1360@gmail.com>
2026-03-24 21:22:09 +0000

    created blank login view

--- homechef/urls.py
+++ homechef/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import index, home
 urlpatterns = [
+    path('start/', index, name = 'index'),
+    path('home/',home, name='home'),
     path('admin/', admin.site.urls),
 ]
