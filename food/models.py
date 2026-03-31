afbfcc3210a9e2a76c5979346de1bf2445f487af
HuseinDawood <h.dawood1360@gmail.com>
2026-03-31 19:27:48 +0100

    created cuisine data model

--- food/models.py
+++ food/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Cuisine(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Cuisines'
+
+    def __str__(self):
+        ordering = ('name',)
+        return self.name 
