408b2a2ad128911b434e955e5e13056a91f3d1e6
HuseinDawood <h.dawood1360@gmail.com>
2026-04-13 20:01:11 +0100

    user preferences model added [14]

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from food.models import Cuisine, Ingredient
+from django.contrib.auth.models import User 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class User_Preference(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_cuisines = models.ManyToManyField(Cuisine,related_name= "preffered_cuisines",blank=True)
+    disliked_cuisines = models.ManyToManyField(Cuisine,related_name= "disliked_cuisines", blank=True)
+    preferred_ingredients = models.ManyToManyField(Ingredient, related_name= "preffered_ingredients", blank=True)
+    disliked_ingredients = models.ManyToManyField(Ingredient, related_name= "disliked_ingredients", blank=True)
+
+    DIET_CHOICES = [
+        ("None", "None"),
+        ("Halal", "Halal"),
+        ("Vegan", "Vegan"),
+        ("Vegetarian", "Vegetarian"),
+    ]
+    
+    dietary_requirements = models.CharField(max_length=20, choices=DIET_CHOICES, default="None")
+    allergies = models.ManyToManyField(Ingredient, related_name="allergies", blank=True)
+
+#having to use related name being a problem
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        User_Preference.objects.create(user=instance)
--- food/admin.py
+++ food/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from . models import Cuisine, Meal, Ingredient
+from accounts.models import User_Preference
 
 admin.site.register(Cuisine)
 admin.site.register(Meal)
 admin.site.register(Ingredient)
+admin.site.register(User_Preference)
